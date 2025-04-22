4606c1a3eef7e77e323c447158d910a5b36ac737
nstddnk <nastya.dudnik80@gmail.com>
2025-04-23 01:44:11 +0200

    revert

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import styles from './Home.module.scss'
-import { Menu } from '../../components/menu/Menu'
-import { TaskBoard } from '../../components/tasks-board/TaskBoard'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Header } from '../../components/header/Header'
+import { ProfileInformation } from '../../components/profile-information/ProfileInformation'
+import { MainInformation } from '../../components/main-information/MainInformation'
+import Employees from '../../components/employees/Employees'
+import Services from '../../components/services/Services'
+import { Feedback } from '../../components/feedback/Feedback'
 
 export const Home = () => {
   return (
     <div className={styles.homeWrapper}>
-      <div className={styles.leftPanel}>
-        <Menu />
-      </div>
-      <div className={styles.rightPanel}>
-        <DndProvider backend={HTML5Backend}>
-          <TaskBoard />
-        </DndProvider>
-      </div>
+      <Header />
+      <main className={styles.main}>
+        <ProfileInformation />
+        <MainInformation />
+        <Employees />
+        <Services />
+        <Feedback />
+      </main>
+      <footer className={styles.footer}></footer>
     </div>
   )
 }
